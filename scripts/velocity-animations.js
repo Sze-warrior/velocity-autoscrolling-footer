cca9f30c56625681392f60a48e7a5b7d9299b9df
Michael Szeto <szetomj@gmail.com>
2017-06-05 18:37:17 +1200

    Initial commit
    Initial commit

--- scripts/velocity-animations.js
+++ scripts/velocity-animations.js
@@ -1,29 +1,10 @@
 window.Velocity = window.Velocity || (window.jQuery || window.Zepto || window).Velocity;
 
-
-//$(document).ready(function(){
-
-    //var introContainer = $('.intro-container').children();
-    //var $endPage = $('#end');
-    //introContainer.velocity({opacity:0}, {duration:1});
-    //introContainer.velocity("transition.expandIn", {stagger:250, delay: 400});
-    //$endPage.velocity("scroll", {introContainer}, {duration:1000, easing:"ease-in"});
-//});
-
-var ending = document.getElementById("end");
-
-/*function scrollAuto(e){
-	Velocity(ending, "scroll", {duration:900, easing:"ease-in"});
-}
-
-window.addEventListener("load", scrollAuto, false); */
-
-var btnTop = document.getElementById("btn-top");
+var btnTop = document.querySelector(".btn-top-footer");
 var header = document.getElementById("top");
 
 function scrollTop(){
-	Velocity(header, "scroll", {duration:800});
-	Velocity(header, "reverse", {duration:800, delay: 1000});
+	Velocity(header, "scroll", {duration:300});
 }	
 
 btnTop.addEventListener("click", scrollTop, false);

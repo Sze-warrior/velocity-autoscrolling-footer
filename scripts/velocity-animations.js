10014225ea591096f4071f556ed897646f5275c7
Michael Szeto <szetomj@gmail.com>
2017-06-28 22:13:25 +1200

    Added new chevron and fixed styling

--- scripts/velocity-animations.js
+++ scripts/velocity-animations.js
@@ -4,7 +4,8 @@ var btnTop = document.querySelector(".btn-top-footer");
 var header = document.getElementById("top");
 
 function scrollTop(){
-	Velocity(header, "scroll", {duration:300});
+	Velocity(header, "scroll", {duration:300}, "ease-in-out");
+	//Velocity(header, "reverse", {duration:300, delay: 100});
 }	
 
 btnTop.addEventListener("click", scrollTop, false);

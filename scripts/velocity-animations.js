0a15f016bdf7f8291d7546645cb262f0b766e1ea
Michael Szeto <szetomj@gmail.com>
2017-07-08 15:24:09 +1200

    Fixed animation timing, cleaned CSS and meta description
    Fixed animation timing, cleaned CSS and meta description

--- scripts/velocity-animations.js
+++ scripts/velocity-animations.js
@@ -4,7 +4,7 @@ var btnTop = document.querySelector(".btn-top-footer");
 var header = document.getElementById("top");
 
 function scrollTop(){
-	Velocity(header, "scroll", {duration:1200, easing: "easeInOut"});
+	Velocity(header, "scroll", {duration:1000, easing:"easeInOut"});
 };	
 
 btnTop.addEventListener("click", scrollTop, false);

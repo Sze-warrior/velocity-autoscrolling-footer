7229373a2350e518d91755d81243aba5e65dd1ea
Michael Szeto <szetomj@gmail.com>
2017-07-18 22:13:02 +1200

    Renamed header var to more accurate name

--- scripts/velocity-animations.js
+++ scripts/velocity-animations.js
@@ -1,10 +1,10 @@
 window.Velocity = window.Velocity || (window.jQuery || window.Zepto || window).Velocity;
 
 var btnTop = document.querySelector(".btn-top-footer");
-var header = document.getElementById("top");
+var scrollTarget = document.getElementById("top");
 
 function scrollTop(){
-	Velocity(header, "scroll", {easing: "ease-in-out",duration:900});
+	Velocity(scrollTarget, "scroll", {easing: "ease-in-out",duration:900});
 };	
 
 btnTop.addEventListener("click", scrollTop, false);
